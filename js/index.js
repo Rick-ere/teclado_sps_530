0046ac621839cad35bf59f3045c89c0b200ad255
Rick <eresisttema@gmail.com>
2022-11-26 05:39:21 -0500

    Final

--- js/index.js
+++ js/index.js
@@ -1,32 +1,83 @@
-// ELEMENTOS
-const tbxContenido = document.getElementById("contenido");
-const btn_cerrar = document.getElementById("btn_cerrar").addEventListener("click", Cerrar);
-let element_temp = null;
+function Init() {
+  // ELEMENTOS
+  let element_temp = null;
+  // const caja = document.getElementById("caja");
+  const tbxContenido = document.getElementById("contenido");
+  const tbxFont_size = document.getElementById("font_size");
+  const tbxFont_bold = document.getElementById("font_bold");
+  const tbxFotn_ancho = document.getElementById("fotn_ancho");
+  const tbxFotn_alto = document.getElementById("fotn_alto");
 
-const GetElement = () => {
+  const tbxSpan_key = document.getElementById("span_key");
+  const tbxSpan_number = document.getElementById("span_number");
+  const cpkColor_fondo = document.getElementById("color_fondo");
+  const cpkColor_letra = document.getElementById("color_letra");
+
+  document.getElementById("btn_cerrar").addEventListener("click", Cerrar);
+
+  function rgbToHex2(a) {
+    if (~a.indexOf("#")) return a;
+    a = a.replace(/[^\d,]/g, "").split(",");
+    return (
+      "#" +
+      ((1 << 24) + (+a[0] << 16) + (+a[1] << 8) + +a[2]).toString(16).slice(1)
+    );
+  }
+
+  const GetScale = (data) => {
+    let scale = { ancho: 1, alto: 1 };
+    if (data != "none" && data != "") {
+      let numb = data.replace("matrix", "");
+      numb = numb.replace("(", "");
+      numb = numb.replace(")", "");
+      numb = numb.split(",");
+
+      for (const key in numb) {
+        if (key == 0) scale.ancho = numb[key].trim();
+        if (key == 3) scale.alto = numb[key].trim();
+      }
+    }
+    return scale;
+  };
+
+  const GetElement = () => {
     console.log("Holaaa");
     const container = document.getElementById("container");
     let number = 1;
 
-    tbxContenido.addEventListener("keyup", GetSetNewValues);
+    tbxContenido.addEventListener("keyup", HandlerGetSetNewValues);
+    tbxFont_size.addEventListener("keyup", HandlerGetSetNewValues);
+    tbxFont_bold.addEventListener("keyup", HandlerGetSetNewValues);
+    tbxFotn_ancho.addEventListener("keyup", HandlerGetSetNewValues);
+    tbxFotn_alto.addEventListener("keyup", HandlerGetSetNewValues);
 
-    config_tecla.forEach(fila => {
-        const div_fila = document.createElement("div");
-        div_fila.className = "fila";
-        div_fila.id = `fila_${fila._id}`;
+    tbxFont_size.addEventListener("change", HandlerGetSetNewValues);
+    tbxFont_bold.addEventListener("change", HandlerGetSetNewValues);
+    tbxFotn_ancho.addEventListener("change", HandlerGetSetNewValues);
+    tbxFotn_alto.addEventListener("change", HandlerGetSetNewValues);
 
-        fila.cols_btn.forEach(col_btn => {
-            const div = GetInitBtn(fila.index, col_btn.index, number, col_btn);
-            div_fila.appendChild(div);
-            number++;
-            col_btn.fontSizeValue
-        });
+    tbxSpan_key.addEventListener("keyup", HandlerGetSetNewValues);
+    tbxSpan_number.addEventListener("keyup", HandlerGetSetNewValues);
+    cpkColor_fondo.addEventListener("change", HandlerGetSetNewValues);
+    cpkColor_letra.addEventListener("change", HandlerGetSetNewValues);
 
-        container.appendChild(div_fila);
+    config_tecla.forEach((fila) => {
+      const div_fila = document.createElement("div");
+      div_fila.className = "fila";
+      div_fila.id = `fila_${fila._id}`;
+
+      fila.cols_btn.forEach((col_btn) => {
+        const div = GetInitBtn(fila.index, col_btn.index, number, col_btn);
+        div_fila.appendChild(div);
+        number++;
+        // col_btn.fontSizeValue;
+      });
+
+      container.appendChild(div_fila);
     });
-}
+  };
 
-const GetInitBtn = (row_index, col_index, number, c_btn) => {
+  const GetInitBtn = (row_index, col_index, number, c_btn) => {
     // console.log(`FILA: ${row_index} COLUMNA: ${col_index} NUMERO: ${number}`);
 
     const div = document.createElement("div");
@@ -35,116 +86,110 @@ const GetInitBtn = (row_index, col_index, number, c_btn) => {
     div.id = `btn_${c_btn._id}`;
     // div.innerText = `${c_btn.value}`;
     if (c_btn.isVisibleNumber) div.appendChild(GetInitSpanNumber(c_btn.number));
+    else div.appendChild(GetInitSpanNumber(""));
     div.appendChild(GetInitP(c_btn));
     div.appendChild(GetInitSpanKey(c_btn.key));
+
     div.addEventListener("click", HandlerClick);
-    //div.addEventListener("", HandlerClick);
-    div.addEventListener("click", (event)=>{
-        event.target.addEventListener("click", (e)=>{
-            console.log("HOLAAAA")
-        })
-    })
-    /*div.addEventListener("click", (event)=>{
-        // event.stopImmediatePropagation();
-        // event.stopPropagation();
-
-        // console.log(event.currentTarget);
-        // console.log(event);
-
-        if(event.target.nodeName == "DIV"){
-            console.log("CUADRO")
-            const parrafo =  event.target.querySelector('p');
-            SetValue(parrafo);
-        }
-        else if(event.target.nodeName == "P"){
-            console.log("PARRAFO");
-            SetValue(event.target);
-        }
-        else if(event.target.nodeName == "SPAN"){
-            console.log("SPAN")
-        }
-    });*/
     return div;
-}
+  };
 
-const GetInitP = (c_btn) => {
+  const GetInitP = (c_btn) => {
     const p = document.createElement("p");
     p.className = "";
     p.style = `${c_btn.valueStyles}`;
     p.innerText = `${c_btn.value}`;
     return p;
-}
+  };
 
-const GetInitSpanNumber = (number) => {
+  const GetInitSpanNumber = (number) => {
     const span = document.createElement("span");
     span.className = "span_number";
     span.id = `span_${number}`;
     span.innerText = `${number}`;
     return span;
-}
+  };
 
-const GetInitSpanKey = (key) => {
+  const GetInitSpanKey = (key) => {
     const span = document.createElement("span");
     span.innerText = `${key}`;
-    span.className = `span_key ${key.length != 1 ? 'font10':'font12'}`;
+    span.className = `span_key ${key.length != 1 ? "font10" : "font12"}`;
     // console.log(key.length)
     return span;
-}
+  };
 
-const HandlerClick = (event) => {
+  const HandlerClick = (event) => {
     // event.stopPropagation();
     // console.log(event.target);
     modal.style.display = "block";
-    if(event.target.nodeName == "DIV"){
-        console.log("CUADRO")
-        const parrafo =  event.target.querySelector('p');
-        SetValues(parrafo);
-        element_temp = parrafo;
+    element_temp = event.currentTarget;
+    SetValues(element_temp);
+  };
+
+  const SetValues = (element) => {
+    // GET
+    const parrafo = element.querySelector("p");
+    const span_key = element.querySelector(".span_key");
+    const span_number = element.querySelector(".span_number");
+
+    // SET
+    tbxContenido.value = parrafo.textContent;
+    tbxFont_size.value = getComputedStyle(parrafo).fontSize.replace("px", "");
+
+    tbxSpan_key.value = span_key.textContent;
+    tbxSpan_number.value = span_number.textContent;
+    tbxFont_bold.value = getComputedStyle(parrafo).fontWeight;
+
+    // matrix(1, 0, 0, 1, 0, 0)
+    let transformacion = getComputedStyle(parrafo).transform;
+    const { ancho, alto } = GetScale(transformacion);
+    tbxFotn_ancho.value = ancho;
+    tbxFotn_alto.value = alto;
+
+    color = getComputedStyle(element).backgroundColor;
+    cpkColor_fondo.value = rgbToHex2(color);
+
+    color = getComputedStyle(parrafo).color;
+    cpkColor_letra.value = rgbToHex2(color);
+  };
+
+  const HandlerGetSetNewValues = (event) => {
+    // console.log(event);
+    let element = null;
+
+    if (event.target == tbxContenido) {
+      element = element_temp.querySelector("p");
+      element.textContent = tbxContenido.value;
+    } else if (event.target == tbxSpan_key) {
+      element = element_temp.querySelector(".span_key");
+      element.textContent = tbxSpan_key.value;
+    } else if (event.target == tbxSpan_number) {
+      element = element_temp.querySelector(".span_number");
+      element.textContent = tbxSpan_number.value;
+    } else if (event.target == cpkColor_fondo) {
+      element_temp.style = `background-color: ${cpkColor_fondo.value}`;
+    } else if (event.target == cpkColor_letra) {
+      element = element_temp.querySelector("p");
+      element.style.color = cpkColor_letra.value;
+    } else if (event.target == tbxFont_size) {
+      element = element_temp.querySelector("p");
+      element.style.fontSize = `${tbxFont_size.value}px`;
+    } else if (event.target == tbxFont_bold) {
+      element = element_temp.querySelector("p");
+      element.style.fontWeight = `${tbxFont_bold.value}`;
+    } else if (event.target == tbxFotn_ancho || event.target == tbxFotn_alto) {
+      element = element_temp.querySelector("p");
+      element.style.transform = `scale(${tbxFotn_ancho.value},${tbxFotn_alto.value})`;
     }
-    else if(event.target.nodeName == "P"){
-        console.log("PARRAFO");
-        SetValues(event.target);
-        element_temp = event.target;
-    }
-    else if(event.target.nodeName == "SPAN"){
-        console.log("SPAN")
-        SetValues(event.target);
-        element_temp = event.target;
-    }
+  };
 
-}
-
-const SetValue = (target)=>{
-    // console.log("ENTRADA", target);
-    const compStyles = window.getComputedStyle(target);
-    // const result = getComputedStyle(target, ":after").content;
-    // console.log("STYLES", compStyles.getPropertyValue('font-size'));
-    // console.log("STYLES2", compStyles);
-
-    // valor = prompt("Ingrese valor: ", target.textContent);
-    // console.log(valor);    
-
-    // if(valor != null && valor != ""){
-    //     target.innerText = valor;
-    //     console.log(target);
-    //     target.style.backgroundColor = "red";
-    // }
-}
-
-const SetValues = (target)=>{
-    tbxContenido.value = target.textContent;
-}
-
-const GetSetNewValues = ()=>{
-    // console.log(e.target);
-    element_temp.textContent = tbxContenido.value;
-}
-
-function Cerrar (event){
+  function Cerrar(event) {
     event.stopPropagation();
     event.stopImmediatePropagation();
     event.preventDefault();
     modal.style.display = "none";
+  }
+  GetElement();
 }
 
-GetElement();
+Init();
--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,39 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let modal = document.getElementById("modal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let btn = document.getElementById("btnOpenModal");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function(event) {
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    modal.style.display = "block";
-}
+let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {    
+btn.onclick = function (event) {
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+
+const position = { x: 0, y: 0 };
+
+interact(".draggable").draggable({
+  listeners: {
+    start(event) {
+    //    console.log(event.type, event.target);
+    //    console.log(event);
+    },
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      event.target.parentNode.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    },
+  },
+});
